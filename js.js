5c537b72d70428b9e3a239c5f66cc81b4ca98e0d
earlgreyed <221594344+earlgreyed@users.noreply.github.com>
2025-08-04 18:39:52 +0300

    getComputerChoice

---
+++ js.js
@@ -0,0 +1,17 @@
+// Return rock, paper or scissors for computer
+function getComputerChoice(string) {
+    let compChoice = Math.floor(Math.random() * 3);
+    if (compChoice === 1) {
+        return console.log("rock");
+    }
+    else if (compChoice === 2) {
+        return console.log("paper");
+    }
+    else {
+        return console.log("scissors");
+    }       
+}
+
+function getHumanChoice(parseInt(prompt(rock, paper, scissors))) {
+    return console.log(parseInt);
+}
