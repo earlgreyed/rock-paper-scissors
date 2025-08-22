58ec262e32d3e694d08274c4c82b04e4fc5d4ea7
earlgreyed <221594344+earlgreyed@users.noreply.github.com>
2025-08-22 12:52:12 +0300

    Added game logic

--- js.js
+++ js.js
@@ -10,47 +10,54 @@ function getComputerChoice() {
     else {
         computerChoice = "scissors";
     }
-    console.log(`computer: ${computerChoice}`);
+    console.log(`hal: ${computerChoice}`);
     return computerChoice;
 }
 
 // Return human choice
-
 function getHumanChoice() {
     let humanChoice = prompt("your choice").toLowerCase();
     console.log(`you: ${humanChoice}`);
     return humanChoice;
 }
 
-// Game
-
-function playGame {
-
-// Scores
 let humanScore = 0;
 let computerScore = 0;
 
-// Round
+
 function playRound(humanChoice, computerChoice) {
     if (computerChoice === "paper" && humanChoice === "scissors" ||
         computerChoice === "rock" && humanChoice === "paper" ||
         computerChoice === "scissors" && humanChoice === "rock"
     ) {
         humanScore++;
-        return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        return console.log(`you win! ${humanChoice} beats ${computerChoice}. score: you ${humanScore} – ${computerScore} hal`);
     }
     else if (computerChoice === humanChoice) {
-        return console.log(`It's a draw`);
+        return console.log(`it's a draw. score: you ${humanScore} – ${computerScore} hal`);
     }
     else {
         computerScore++;
-        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        return console.log(`you lose! ${computerChoice} beats ${humanChoice}. score: you ${humanScore} – ${computerScore} hal`);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+function playGame() {
+    for (let counter = 1; counter < 5; counter++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    if (humanScore > computerScore) {
+        alert(`you win! final score: you ${humanScore} – ${computerScore} hal`)
+    }
+    else if (humanScore < computerScore) {
+       alert(`you lose! final score: you ${humanScore} – ${computerScore} hal`)
+    }
+    else {
+        alert(`it's a draw! final score: you ${humanScore} – ${computerScore} hal`)
+    }
 
 }
+
+playGame();
