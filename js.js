50f77d1d952121c11c2c0ca88879b6c0036db204
earlgreyed <221594344+earlgreyed@users.noreply.github.com>
2025-08-22 12:24:25 +0300

    Added round logic

--- js.js
+++ js.js
@@ -1,17 +1,56 @@
 // Return rock, paper or scissors for computer
-function getComputerChoice(string) {
-    let compChoice = Math.floor(Math.random() * 3);
-    if (compChoice === 1) {
-        return console.log("rock");
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 1) {
+        computerChoice = "rock";
     }
-    else if (compChoice === 2) {
-        return console.log("paper");
+    else if (computerChoice === 2) {
+        computerChoice = "paper";
     }
     else {
-        return console.log("scissors");
-    }       
+        computerChoice = "scissors";
+    }
+    console.log(`computer: ${computerChoice}`);
+    return computerChoice;
 }
 
-function getHumanChoice(parseInt(prompt(rock, paper, scissors))) {
-    return console.log(parseInt);
+// Return human choice
+
+function getHumanChoice() {
+    let humanChoice = prompt("your choice").toLowerCase();
+    console.log(`you: ${humanChoice}`);
+    return humanChoice;
+}
+
+// Game
+
+function playGame {
+
+// Scores
+let humanScore = 0;
+let computerScore = 0;
+
+// Round
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === "paper" && humanChoice === "scissors" ||
+        computerChoice === "rock" && humanChoice === "paper" ||
+        computerChoice === "scissors" && humanChoice === "rock"
+    ) {
+        humanScore++;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+    else if (computerChoice === humanChoice) {
+        return console.log(`It's a draw`);
+    }
+    else {
+        computerScore++;
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 }
